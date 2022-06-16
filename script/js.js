c54f664696c0c1f7f329dacf37aba431a7f5fa4b
ErpePpp <91189978+Erpe12@users.noreply.github.com>
2022-06-16 14:09:42 +1000

    Update js.js

--- script/js.js
+++ script/js.js
@@ -1,3 +1,4 @@
+/* Swiper */
 var swiper = new Swiper(".mySwiper", {
 	spaceBetween: 30,
 	pagination: {
@@ -7,13 +8,16 @@ var swiper = new Swiper(".mySwiper", {
   });
 
 
-$(function(){
-	$("#menuToggle").on("click", function(){
-		$("#menu").toggleClass("menu-open");
-		$("body").toggleClass("hidden");
-	});
-	$(".page_name").on("click", function(){
-		$("#menu").toggleClass("menu-open");
-		$("body").toggleClass("hidden");
-	});
-})(jQuery);
+/* Open menu */
+let searcMenu = document.querySelector("#menu"),
+	searchPageName = document.querySelector(".page_name");
+
+	searcMenu.addEventListener('click', () => {
+	document.querySelector("#menu").classList.toggle("menu-open");
+	document.querySelector("body").classList.toggle("hidden");
+});
+
+	searchPageName.addEventListener('click', ()=> {
+	document.querySelector("#menu").classList.toggle("menu-open");
+	document.querySelector("body").classList.toggle("hidden");
+})
