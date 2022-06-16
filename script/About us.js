73425534035c80d2e5de772da557f1f27bfeb89b
ErpePpp <91189978+Erpe12@users.noreply.github.com>
2022-06-16 14:10:01 +1000

    Update About us.js

--- script/About us.js
+++ script/About us.js
@@ -1,11 +1,13 @@
-$(function(){
-	$("#menuToggle").on("click", function(){
-		$("#menu").toggleClass("menu-open");
-		$("body").toggleClass("hidden");
-	});
-	$(".page_name").on("click", function(){
-		$("#menu").toggleClass("menu-open");
-		$("body").toggleClass("hidden");
-	});
-})(jQuery);
+/* Open menu */
+let searcMenu = document.querySelector("#menu"),
+	searchPageName = document.querySelector(".page_name");
 
+	searcMenu.addEventListener('click', () => {
+	document.querySelector("#menu").classList.toggle("menu-open");
+	document.querySelector("body").classList.toggle("hidden");
+});
+
+	searchPageName.addEventListener('click', ()=> {
+	document.querySelector("#menu").classList.toggle("menu-open");
+	document.querySelector("body").classList.toggle("hidden");
+});
